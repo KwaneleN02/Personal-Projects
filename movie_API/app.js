802a4f9af5dda9234d922a55b42a99730b4fe3c5
KwaneleN02 <54561549+KwaneleN02@users.noreply.github.com>
2022-07-05 14:03:42 -0600

    Add files via upload

---
+++ movie_API/app.js
@@ -0,0 +1,77 @@
+const API_URL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dab08741be0cc9c0299f20ab1b9f02f8&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
+
+const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=dab08741be0cc9c0299f20ab1b9f02f8&query="';
+
+const form = document.querySelector('form');
+const search = document.querySelector('.search');
+const main = document.querySelector('main');
+
+
+getMovies(API_URL);
+
+async function getMovies(url)
+{
+    const res = await fetch(url)
+    const data = await res.json()
+    showMovies(data.results);
+}
+
+function showMovies(movies)
+{
+    main.innerHTML = '';
+    movies.forEach((movie)=>
+    {
+        const{title, poster_path, vote_average, overview } = movie;
+        const movieElement = document.createElement('div');
+        movieElement.className = 'movie';
+        movieElement.innerHTML = 
+        `
+                <img src="${IMG_PATH + poster_path}" alt=" ${title}">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${classByRate(vote_average)}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>${title}</h3>
+                   ${overview}
+                </div>
+            ` 
+            main.appendChild(movieElement);
+    });
+}
+
+
+function classByRate(vote)
+{
+    if(vote >= 8)
+    {
+        return 'green';
+    }
+    else if(vote >= 5)
+    {
+        return 'orange';
+    }
+    else
+    {
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm && searchTerm !=='')
+    {
+        getMovies(SEARCH_URL + searchTerm)
+
+        search.value = '';
+    }
+    else
+    {
+        window.location.reload();
+    }
+})
